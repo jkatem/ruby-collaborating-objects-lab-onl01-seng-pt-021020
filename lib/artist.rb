2f3f8e58a0fd6e4e33b841ef0295713e469baae6
Jen Miao <jenmiao@gmail.com>
2020-03-11 03:52:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,28 +39,28 @@
 # end
 
 # class Artist
-# 
+#
 #   attr_accessor :name, :songs
 #   @@all = []
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @songs = []
 #     save
 #   end
-# 
+#
 #   def add_song(song)
 #     @songs << song
 #   end
-# 
+#
 #   def save
 #     @@all << self #saves the artist name to the all array
 #   end
-# 
+#
 #   def self.all
 #     @@all   #displays the all array
 #   end
-# 
+#
 #   def self.find_or_create_by_name(artist_name)
 #     found_artist = self.all.find {|artist| artist.name == artist_name}
 #     if found_artist
@@ -71,7 +71,7 @@
 #       new_artist
 #     end
 #   end
-# 
+#
 #   def print_songs
 #     @songs.each do |song|
 #       puts song.name
